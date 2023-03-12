6c2e6dbb54ecf20b219185241e5a1d63adb57c58
felipematheus1337 <lipehbr@gmail.com>
2023-03-12 18:56:12 -0300

    Projeto iniciado!

---
+++ pagamentos-ms/src/test/java/br/compagamentosms/PagamentosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.compagamentosms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
